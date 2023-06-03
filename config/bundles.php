ac24e9c8451cecab4044f5d1ae8cce500a09aeea
Léo Moille <leomoille18@gmail.com>
2023-06-03 16:29:21 +0200

    :passport_control:  Adding user auth with JWT

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
